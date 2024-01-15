cc7d1b317ecfc2ad421705bf690c83ea3269ef0e
Kitselyuk Egor <kitselyuk@mindbox.cloud>
2024-01-15 16:35:08 +0300

    Add Mindbox integration

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState }  from 'react';
+import MindboxSdk, { LogLevel } from "mindbox-sdk";
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [uuid, setUuid] = useState('');
+  const [token, setToken] = useState('');
+
+  const callback = useCallback((pushUrl, pushPayload) => {
+    setTimeout(() => {
+      Alert.alert("Push Notification Data", `${pushUrl}\n${pushPayload}`);
+    }, 600);
+
+    console.log("onPushClickReceived");
+  }, []);
+
+  const appInitializationCallback = useCallback(async () => {
+      const configuration = {
+        domain: 'api.mindbox.ru',
+        endpointId: 'pushok-rn-ios-sandbox',
+        subscribeCustomerIfCreated: true,
+        shouldCreateCustomer: true,
+      };
+      MindboxSdk.setLogLevel(LogLevel.DEBUG);
+      await MindboxSdk.initialize(configuration);
+
+      MindboxSdk.onPushClickReceived(callback);
+
+      MindboxSdk.getDeviceUUID((uuid) => { 
+        console.log(`getDeviceUUID ${uuid}`);
+        setUuid(uuid)
+      });
+
+      MindboxSdk.getToken((token) => { 
+        console.log(`getToken ${token}`);
+        setToken(token)
+      });
+
+      return () => {
+        MindboxSdk.removeOnPushClickReceived();
+      };
+
+  }, []).call();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Mindbox</Text>
       <StatusBar style="auto" />
+      <Text>UUID {uuid}</Text>
+      <Text>Token {token}</Text>
     </View>
   );
 }
@@ -19,3 +61,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
--- ios/mindboxrndemoapp/AppDelegate.mm
+++ ios/mindboxrndemoapp/AppDelegate.mm
@@ -1,17 +1,48 @@
 #import "AppDelegate.h"
 
+@import Mindbox;
+@import MindboxSdk;
+
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
 
+
 @implementation AppDelegate
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"main";
-
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+//  [[Mindbox logger] setLogLevel:LogLevelDebug];
+  
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    center.delegate = self;
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+      if (!error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+          [[UIApplication sharedApplication] registerForRemoteNotifications];
+          [[Mindbox shared] notificationsRequestAuthorizationWithGranted:granted];
+        });
+      }
+      else {
+        NSLog(@"NotificationsRequestAuthorization failed with error: %@", error.localizedDescription);
+      }
+    }];
+  
+  TrackVisitData *data = [[TrackVisitData alloc] init];
+    data.launchOptions = launchOptions;
+    [[Mindbox shared] trackWithData:data];
+  
+  if (@available(iOS 13.0, *)) {
+        [[Mindbox shared] registerBGTasks];
+      }
+      else {
+        [[UIApplication sharedApplication] setMinimumBackgroundFetchInterval:UIApplicationBackgroundFetchIntervalMinimum];
+      }
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
@@ -19,39 +50,44 @@
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry"];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
-// Linking API
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-  return [super application:application openURL:url options:options] || [RCTLinkingManager application:application openURL:url options:options];
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+  //print(@"Failed to register for remote notifications: \(error.localizedDescription)");
+   NSLog(@"Failed to register for remote notifications: %@", error.localizedDescription);
 }
 
-// Universal Links
-- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
-  BOOL result = [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
-  return [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler] || result;
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+  completionHandler(UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionBadge);
 }
 
-// Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
-  return [super application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  [[Mindbox shared] apnsTokenUpdateWithDeviceToken:deviceToken];
 }
 
-// Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
-{
-  return [super application:application didFailToRegisterForRemoteNotificationsWithError:error];
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler {
+
+  [[Mindbox shared] pushClickedWithResponse:response];
+  [MindboxJsDelivery emitEvent:response];
+  TrackVisitData *data = [[TrackVisitData alloc] init];
+    data.push = response;
+    [[Mindbox shared] trackWithData:data];
+  completionHandler();
 }
 
-// Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
-{
-  return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+  TrackVisitData *data = [[TrackVisitData alloc] init];
+  data.universalLink = userActivity;
+  [[Mindbox shared] trackWithData:data];
+  return YES;
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+  [[Mindbox shared] application:application performFetchWithCompletionHandler:completionHandler];
 }
 
 @end
